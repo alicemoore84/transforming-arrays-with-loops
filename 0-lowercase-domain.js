f967805c0d17a65b6f8396226486c1accfa7a581
Alice Moore <meepitymoopity84@gmail.com>
2022-10-27 15:21:05 +0100

    pseudocode created

---
+++ 0-lowercase-domain.js
@@ -0,0 +1,13 @@
+/*
+function: lowercase
+parameters: inputArray
+
+
+function lowercase(inputArray)
+    DECLARE lowercaseArray = [];
+    for (let string of inputArray)
+        string = string.toLowerCase();
+        lowercaseArray.push[string];
+
+    return lowercaseArray
+*/
